4f1219549254bdb6a7ebd7772603138b6a9a79c3
Susanna Rokka <susannaajrokka@gmail.com>
2025-01-19 22:26:27 +0100

    Två maträtter går att matcha till viner

--- MatchingLogic.cs
+++ MatchingLogic.cs
@@ -11,33 +11,33 @@ public class FoodCategory
     public static void RunFoodCategory()
     {
         Console.WriteLine("Välj en matkategori: ");
-        Console.WriteLine("1. Kött (nöt, fågel, vilt osv.): ");
-        Console.WriteLine("2. Fisk och skaldjur: ");
-        Console.WriteLine("3. Soppa, sallader och kalla förrätter: ");
-        Console.WriteLine("4. Grönsaksbaserade rätter: ");
-        Console.WriteLine("5. Ostar: ");
-        Console.WriteLine("6. Dessert och sötsaker: \n");
+       // Console.WriteLine("1. Kött (nöt, fågel, vilt osv.): ");
+       // Console.WriteLine("2. Fisk och skaldjur: ");
+        Console.WriteLine("3. Soppa, sallader och kalla förrätter: (VÄLJ NUMMER - 11 - FÖR ATT TESTA)");
+        Console.WriteLine("4. Grönsaksbaserade rätter: (VÄLJ NUMMER - 25 - FÖR ATT TESTA)");
+       // Console.WriteLine("5. Ostar: ");
+      //  Console.WriteLine("6. Dessert och sötsaker: \n");
         string? choice = Console.ReadLine().ToUpper();
 
          switch (choice)
         {
                 case "1":
-                    //MeatCategory();
+                    //MeatCategory();  //tbc under utveckling
                     break;
                 case "2":
-                    //FishCategory();
+                    //FishCategory(); //tbc under utveckling
                     break;
                 case "3":
-                    //SoupSaladCategory();
+                    SoupSaladCategory.GetSoupSaladFoodCategory();
                     break;
                 case "4":
                     VegetableCategory.GetVegetableFoodCategory();
                     break;
                 case "5":
-                    //CheeseCategory();
+                    //CheeseCategory(); //tbc under utveckling
                     break;
                 case "6":
-                    //DessertCategory();
+                    //DessertCategory(); //tbc under utveckling
                     break;
                 default:
                     break;
@@ -45,8 +45,10 @@ public class FoodCategory
             
     }
 }
-        
- /*public class MatchingLogic
+    // Planer att refaktorera och skapa en egen klass för varje underkategori
+    // samt se till att logiken separeras för sig.
+
+ /*public class MatchingLogic 
  {
     public static void RunMatchingLogic()
     {
@@ -55,7 +57,84 @@ public class FoodCategory
         {
             connection.Open();
         }}}*/
+public class SoupSaladCategory
+{
+    public static void GetSoupSaladFoodCategory()
+    {
+        var repository = new DatabaseRepository();
+        using (IDbConnection connection = repository.Connect())
+        {
+            connection.Open();
 
+            string sqlQuery = @"
+            SELECT f.FoodID, f.Name AS FoodName
+            FROM Food f
+            WHERE f.Category = 'Salad';";
+
+            try
+            {
+                var foodCategory = connection.Query(sqlQuery);
+                Console.WriteLine("Välj en maträtt (nr.) du vill matcha:");
+                    foreach (var row in foodCategory)
+                    {
+                        Console.WriteLine($"Maträtt nr: {row.FoodID}, Maträtt: {row.FoodName}");
+                    }
+
+                    Console.WriteLine("Välj en maträtt (nr.) du vill matcha:");
+                    string? choice = Console.ReadLine();
+
+                    if (int.TryParse(choice, out int selectedFoodID))
+                    {
+                                // Query for matching wine
+                            string wineQuery = @"
+                            SELECT G.Name, W.Producer
+                            FROM Food F
+                            JOIN WineToFood WF ON F.FoodID = WF.FoodID
+                            JOIN Wine W ON WF.WineID = W.WineID
+                            JOIN GrapeToWine GW ON W.WineID = GW.WineID
+                            JOIN Grape G ON GW.GrapeID = G.GrapeID
+                            WHERE F.FoodID = @FoodID;";
+
+                    
+                            string flavourQuery = "SELECT * FROM GetFlavourNamesForFood(@FoodID);";
+                            var flavours = connection.Query(flavourQuery, new { FoodID = selectedFoodID });
+                            var FlavourNames = string.Join(",", flavours.Select(row => row.FlavourName));
+                            Console.WriteLine("Denna maträtt har följande ingredienser och smaker:");
+                            foreach (var row in flavours)
+                            {
+                                Console.WriteLine($"{row.FlavourName}");
+                            }
+                            //tbc. Här skall 'Notes' från viner hämtas för att visa matchning
+
+                            /*string notesQuery = "SELECT * FROM GetNotesCategoriesForGrape(@GrapeID);";
+                            var notes = connection.Query(notesQuery, new { GrapeID = row.GrapeID }).ToList();
+                            var NoteNames = string.Join(",", notes.Select(row => row.NotesName));
+                            Console.WriteLine("Därför vill man gärna matcha med följande toner i ett vin:");
+                            foreach (var row in notes)
+                            {
+                                Console.WriteLine($"{row.NotesName}");
+                            }*/
+                            var results = connection.Query(wineQuery, new { FoodID = selectedFoodID });
+                            Console.WriteLine("Därför matchar denna maträtt med följande druva: ");
+                        
+                            foreach (var row in results)
+                            {
+                                Console.Write($"{row.Name}. \n");
+                                Console.WriteLine($"Prova gärna vin från denna producent: {row.Producer}");
+                            }
+                    }
+                    else
+                    {             
+                        Console.WriteLine("Felaktig inmatning, försök igen.");
+                    }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Oj det blev fel någonstans. Error: {ex.Message}");
+            }
+        }
+    }
+}
 public class VegetableCategory
 {
     public static void GetVegetableFoodCategory()
@@ -93,13 +172,22 @@ public class VegetableCategory
                 JOIN Grape G ON GW.GrapeID = G.GrapeID
                 WHERE F.FoodID = @FoodID;";
 
-            var results = connection.Query(wineQuery, new { FoodID = selectedFoodID });
+                string flavourQuery = "SELECT * FROM GetFlavourNamesForFood(@FoodID);";
+                var flavours = connection.Query(flavourQuery, new { FoodID = selectedFoodID });
+                var FlavourNames = string.Join(",", flavours.Select(row => row.FlavourName));
+                Console.WriteLine("Denna maträtt har följande ingredienser och smaker:");
+                foreach (var row in flavours)
+                {
+                    Console.WriteLine($"{row.FlavourName}");
+                }
+                var results = connection.Query(wineQuery, new { FoodID = selectedFoodID });
 
-            Console.WriteLine("Denna maträtt matchar med följande druva: ");
-            foreach (var row in results)
-            {
-                Console.WriteLine($"{row.Name}. Prova gärna vin från denna producent: {row.Producer}");
-            }
+                Console.Write("Denna maträtt matchar därför med följande druva: ");
+                foreach (var row in results)
+                {
+                    Console.WriteLine($"{row.Name}");
+                    Console.WriteLine($"Prova gärna vin från denna producent: {row.Producer} \n");
+                }
                 }
                 else
                 {
